35a2e45f44d5fc80211f1c380baefb376f5cddca
code-250 <richmunye@gmail.com>
2021-02-21 02:08:37 +0200

    done with setting up our environment

--- Src/App/Controllers/authController.js
+++ Src/App/Controllers/authController.js
@@ -1,8 +1,7 @@
 import User from "../../Database/models/UserModels.js";
-<<<<<<< HEAD
-import { secretKey } from "../../Database/Services/system/authConfig.js";
-=======
->>>>>>> c54a3f426cdc7091a4c846272236f792a9d12d75
+
+import { secretKey } from "../../Database/system/authConfig.js";
+
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import Post from "../../Database/models/PostsModel.js";
@@ -10,13 +9,8 @@ import Post from "../../Database/models/PostsModel.js";
 const Op = User.Sequelize.Op;
 export const signUp = async (request, response) => {
   // save User to database
-
-<<<<<<< HEAD
-  User.create({
-=======
   await User.create({
->>>>>>> c54a3f426cdc7091a4c846272236f792a9d12d75
-    username: request.body.username,
+
     email: request.body.email,
     password: bcrypt.hashSync(request.body.password, 8),
   });
