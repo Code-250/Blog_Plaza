6556541be96471d854637dcf22bcea59691e4838
code-250 <richmunye@gmail.com>
2021-03-05 14:02:54 +0200

    user authentication and routes protection

--- Src/App/Routes/Auth/userRoute.js
+++ Src/App/Routes/Auth/userRoute.js
@@ -1,59 +1,10 @@
 import { Router } from "express";
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> b23f900 (all apis)
-=======
->>>>>>> 239248d (user authentication and routes protection)
-=======
->>>>>>> 239248d (user authentication and routes protection)
 import { signUp, Login, deleteUser } from "../../Controllers/userController.js";
 
 const UserRouter = Router();
 
 UserRouter.post("/", signUp);
 UserRouter.post("/login", Login);
-<<<<<<< HEAD
-=======
-=======
->>>>>>> bf8cc44 (managed tu create users)
-import {
-  createUser,
-  getAllUser,
-  deleteUser,
-} from "../../Controllers/userController.js";
-
-const UserRouter = Router();
-
-UserRouter.post("/", createUser);
-UserRouter.get("/", getAllUser);
-<<<<<<< HEAD
->>>>>>> bf8cc44 (managed tu create users)
-=======
-import { signUp, Login, deleteUser } from "../../Controllers/userController.js";
-
-const UserRouter = Router();
-
-UserRouter.post("/", signUp);
-UserRouter.post("/login", Login);
->>>>>>> aed9245 (user authentication and routes protection)
-=======
->>>>>>> b23f900 (all apis)
-=======
->>>>>>> bf8cc44 (managed tu create users)
-=======
-import { signUp, Login, deleteUser } from "../../Controllers/userController.js";
-
-const UserRouter = Router();
-
-UserRouter.post("/", signUp);
-UserRouter.post("/login", Login);
->>>>>>> aed9245 (user authentication and routes protection)
 UserRouter.delete("/:id", deleteUser);
 
 export default UserRouter;
